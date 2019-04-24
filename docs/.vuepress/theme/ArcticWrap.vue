977ba6900ac851f6515b344264bf67fb833f384e
v_chenchao <2020807070@qq.com>
2019-04-24 19:44:17 +0800

    feat: add some file

--- docs/.vuepress/theme/ArcticWrap.vue
+++ docs/.vuepress/theme/ArcticWrap.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>
-    <div class="atc-item" v-for="item in posts">
-      <router-link :to="item.path"><p v-if="isAtc(item.path)">{{ item.title }}</p></router-link>
+  <div class="arctic-wrap">
+    <div class="arctic-flow">
+      <div class="atc-item" v-for="item in posts">
+        <router-link :to="item.path"><p v-if="isAtc(item.path)">{{ item.title }}</p></router-link>
+      </div>
     </div>
-    <!-- 分页器 -->
+    <div class="arctic-other">
+      121321321321
+    </div>
   </div>
+    <!-- 分页器 -->
+
 </template>
 
 <script>
@@ -31,3 +37,16 @@ export default {
   },
 }
 </script>
+
+<style lang="stylus">
+.arctic-wrap
+  display flex
+  .arctic-flow
+    flex: 1
+  .arctic-flow
+    background #ccc
+  .arctic-other
+    width 300px
+    margin-left 20px
+    background #ccc
+</style>
